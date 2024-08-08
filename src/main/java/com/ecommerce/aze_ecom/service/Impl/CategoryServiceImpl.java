a12320a567162acc317a371ed523129d55fdee56
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-08 12:38:24 +0100

    adding Service of category

---
+++ src/main/java/com/ecommerce/aze_ecom/service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ecommerce.aze_ecom.service.Impl;
+
+import com.ecommerce.aze_ecom.beans.Category;
+import com.ecommerce.aze_ecom.service.Interf.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public List<Category> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+
+    }
+}
---
+++ src/main/java/com/ecommerce/aze_ecom/service/Interf/CategoryService.java
@@ -0,0 +1,10 @@
+package com.ecommerce.aze_ecom.service.Interf;
+
+import com.ecommerce.aze_ecom.beans.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
