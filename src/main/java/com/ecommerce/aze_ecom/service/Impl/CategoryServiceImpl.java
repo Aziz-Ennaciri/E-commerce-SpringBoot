dc1d564446d9f44fa7e4edf927298483755b2f8d
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-10 23:08:09 +0100

    updating  CategoryServiceImpl #2

--- src/main/java/com/ecommerce/aze_ecom/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/aze_ecom/service/Impl/CategoryServiceImpl.java
@@ -28,30 +28,34 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long categoryId) {
-        List<Category> categories = categoryDao.findAll();
-        Category category = categories.stream()
-                .filter(c->c.getCategoryId().equals(categoryId)).findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        if (category==null)
-            return "Category not found";
-
-
-        categoryDao.delete(category);
+        Category savedCategory = categoryDao.findById(categoryId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+                categoryDao.delete(savedCategory);
         return "the category"+categoryId+"has been deleted";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryDao.findAll();
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if (optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return categoryDao.save(existingCategory);
-        }
-        else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+//        method 1
+        Category savedCategory = categoryDao.findById(categoryId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        category.setCategoryId(categoryId);
+        savedCategory=categoryDao.save(category);
+        return savedCategory;
+
+
+
+
+//        this method 2
+//        List<Category> categories = categoryDao.findAll();
+//        Optional<Category> optionalCategory = categories.stream()
+//                .filter(c -> c.getCategoryId().equals(categoryId))
+//                .findFirst();
+//        if (optionalCategory.isPresent()){
+//            Category existingCategory = optionalCategory.get();
+//            existingCategory.setCategoryName(category.getCategoryName());
+//            return categoryDao.save(existingCategory);
+//        }
+//        else{
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
     }
 }
