20e67eb72ccc5c841aed262411231ac4b266ab24
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-10 20:02:12 +0100

    adding ny categoryDAO (Data Access Object)

---
+++ src/main/java/com/ecommerce/aze_ecom/dao/CategoryDao.java
@@ -0,0 +1,7 @@
+package com.ecommerce.aze_ecom.dao;
+
+import com.ecommerce.aze_ecom.beans.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryDao extends JpaRepository<Category,Long> {
+}
