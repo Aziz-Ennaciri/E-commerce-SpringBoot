b9f55a0dabdc5cd3bcf50743dfbc0fc3362e11c3
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-11 21:02:57 +0100

    add mor validation annotations

--- src/main/java/com/ecommerce/aze_ecom/beans/Category.java
+++ src/main/java/com/ecommerce/aze_ecom/beans/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +16,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
     @NotBlank
+    @Size(min = 5,message = "Category name must contains at least 5 characters")
     private String categoryName;
 
     public Category() {
