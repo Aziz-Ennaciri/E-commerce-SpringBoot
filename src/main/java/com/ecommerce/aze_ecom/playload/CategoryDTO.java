0065320362b7419cfda0a918e696aa72c444523f
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-13 20:27:15 +0100

    add categoryDTO for implementing concept of pagination

---
+++ src/main/java/com/ecommerce/aze_ecom/playload/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.ecommerce.aze_ecom.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private Long categoryId;
+    private String categoryName;
+}
