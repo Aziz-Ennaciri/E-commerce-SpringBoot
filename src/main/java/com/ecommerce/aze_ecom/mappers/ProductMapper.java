fb2ace22d6ed734eb010a457f2b77c2ea7945563
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-18 18:43:12 +0100

    creating mapper for product and productDto

---
+++ src/main/java/com/ecommerce/aze_ecom/mappers/ProductMapper.java
@@ -0,0 +1,33 @@
+package com.ecommerce.aze_ecom.mappers;
+
+import com.ecommerce.aze_ecom.beans.Product;
+import com.ecommerce.aze_ecom.playload.ProductDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+
+    public Product toEntity(ProductDTO productDTO) {
+        Product product = new Product();
+        product.setProductId(productDTO.getProductId());
+        product.setProductName(productDTO.getProductName());
+        product.setImage(productDTO.getImage());
+        product.setQuantity(productDTO.getQuantity());
+        product.setPrice(productDTO.getPrice());
+        product.setDiscount(productDTO.getDiscount());
+        product.setSpecialPrice(productDTO.getSpecialPrice());
+        return product;
+    }
+
+    public ProductDTO toDto(Product product) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductId(product.getProductId());
+        productDTO.setProductName(product.getProductName());
+        productDTO.setImage(product.getImage());
+        productDTO.setQuantity(product.getQuantity());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setDiscount(product.getDiscount());
+        productDTO.setSpecialPrice(product.getSpecialPrice());
+        return productDTO;
+    }
+}
