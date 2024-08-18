31f332a86aeed765e13cc006f998210ee8d234db
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-18 20:42:33 +0100

    adding query in ProductDAO to find Products by Category

--- src/main/java/com/ecommerce/aze_ecom/dao/ProductDAO.java
+++ src/main/java/com/ecommerce/aze_ecom/dao/ProductDAO.java
@@ -1,9 +1,13 @@
 package com.ecommerce.aze_ecom.dao;
 
+import com.ecommerce.aze_ecom.beans.Category;
 import com.ecommerce.aze_ecom.beans.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductDAO extends JpaRepository<Product,Long> {
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
