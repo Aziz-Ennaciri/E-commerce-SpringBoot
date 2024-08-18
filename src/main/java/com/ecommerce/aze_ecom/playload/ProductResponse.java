b34ec3f39130057923e029d0c329cd30544bd9f5
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-18 18:44:51 +0100

    creating productResponse for list of ProductDTO

---
+++ src/main/java/com/ecommerce/aze_ecom/playload/ProductResponse.java
@@ -0,0 +1,15 @@
+package com.ecommerce.aze_ecom.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private List<ProductDTO> content;
+
+}
