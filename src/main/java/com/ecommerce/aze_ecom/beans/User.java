45478a11c2da8c7f994286ef9f152f4d396296ff
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-24 20:23:35 +0100

    add relationship between User and Role , and add some specification for the table and uniqueConstraint

--- src/main/java/com/ecommerce/aze_ecom/beans/User.java
+++ src/main/java/com/ecommerce/aze_ecom/beans/User.java
@@ -5,29 +5,49 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username"),
+                                            @UniqueConstraint(columnNames = "email")
+                                           })
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max = 20)
+    @Column(name = "username")
     private String userName;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
 
     @NotBlank
     @Size(max = 50)
     @Email
-    private String password;
-
-    @NotBlank
-    @Size(max = 120)
+    @Column(name = "email")
     private String email;
+
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "role_user",joinColumns = @JoinColumn
+                                    (name = "user_id"),inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
 
     public User(String userName, String password, String email) {
         this.userName = userName;
