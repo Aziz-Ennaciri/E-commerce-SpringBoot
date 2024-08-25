ff710a7d3dfdcfd23c905777c56e25d0250f4436
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-25 15:28:54 +0100

    adding MessageResponse and UserInfoResponse for customization JWT

---
+++ src/main/java/com/ecommerce/aze_ecom/Security/response/UserInfoResponse.java
@@ -0,0 +1,52 @@
+package com.ecommerce.aze_ecom.Security.response;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    private Long id;
+    private String jwtToken;
+    private String username;
+    private List<String> roles;
+
+    public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+        this.jwtToken = jwtToken;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
+
+
+
