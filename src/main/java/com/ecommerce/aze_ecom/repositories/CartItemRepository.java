a1206065e062a3df80151976aa71eccea8eeb96c
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-27 19:54:23 +0100

    adding queries for both of these repositories CartRepository and CartItemRepository

--- src/main/java/com/ecommerce/aze_ecom/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/CartItemRepository.java
@@ -2,8 +2,11 @@ package com.ecommerce.aze_ecom.repositories;
 
 import com.ecommerce.aze_ecom.beans.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
@@ -2,8 +2,11 @@ package com.ecommerce.aze_ecom.repositories;
 
 import com.ecommerce.aze_ecom.beans.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
