5ad31f568c93e11815a13a2101176b539e6c66bd
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-27 02:10:08 +0100

    creating two beans Cart and Cart Items

---
+++ src/main/java/com/ecommerce/aze_ecom/beans/Cart.java
@@ -0,0 +1,29 @@
+package com.ecommerce.aze_ecom.beans;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE},orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    private Double totalPrice = 0.0;
+}
---
+++ src/main/java/com/ecommerce/aze_ecom/beans/CartItem.java
@@ -0,0 +1,29 @@
+package com.ecommerce.aze_ecom.beans;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="cart_items" )
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private Double discount;
+    private Double productPrice;
+ }
