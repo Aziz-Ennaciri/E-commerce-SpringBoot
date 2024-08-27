6eade512f638759c001813d13882c01fe35111ae
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-27 19:57:42 +0100

    creating CartController and add api PostMapping  to add product to cart

---
+++ src/main/java/com/ecommerce/aze_ecom/controller/CartController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.aze_ecom.controller;
+
+import com.ecommerce.aze_ecom.playload.CartDTO;
+import com.ecommerce.aze_ecom.service.Interf.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+}
