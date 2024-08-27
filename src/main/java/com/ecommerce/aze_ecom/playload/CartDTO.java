b688cee69409e6c792097b8cd4e9d3e1bb27cc97
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-27 16:55:54 +0100

    creating CartDTO and CartItemDTO

---
+++ src/main/java/com/ecommerce/aze_ecom/playload/CartDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.aze_ecom.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDTO {
+    private Long cartId;
+    private Double totalPrice = 0.0;
+    private List<ProductDTO> productDTOS = new ArrayList<>();
+}
---
+++ src/main/java/com/ecommerce/aze_ecom/playload/CartItemDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.aze_ecom.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+    private Long cartItemId;
+    private CartDTO cartDTO;
+    private ProductDTO productDTO;
+    private Integer quantity;
+    private Double discount;
+    private Double productPrice;
+}
