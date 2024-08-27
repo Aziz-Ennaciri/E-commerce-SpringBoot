cd14f5a0b9157bcd9b1d97e4984daaf7200b05b8
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-27 19:55:08 +0100

    creating authUtil to retrieve details of the user who already authenticate

---
+++ src/main/java/com/ecommerce/aze_ecom/Util/AuthUtil.java
@@ -0,0 +1,43 @@
+package com.ecommerce.aze_ecom.Util;
+
+import com.ecommerce.aze_ecom.beans.User;
+import com.ecommerce.aze_ecom.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String loggedInEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getUserId();
+    }
+
+    public User loggedInUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+        return user;
+
+    }
+
+
+}
