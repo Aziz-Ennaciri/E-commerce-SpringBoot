bfae64be0b2a77ecc9c6eeb8017650fc614707fa
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 00:22:40 +0100

    add Get Carts to an api

--- src/main/java/com/ecommerce/aze_ecom/controller/CartController.java
+++ src/main/java/com/ecommerce/aze_ecom/controller/CartController.java
@@ -5,10 +5,9 @@ import com.ecommerce.aze_ecom.service.Interf.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +15,12 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getCarts() {
+        List<CartDTO> cartDTOs = cartService.getAllCarts();
+        return new ResponseEntity<List<CartDTO>>(cartDTOs, HttpStatus.FOUND);
+    }
 
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
