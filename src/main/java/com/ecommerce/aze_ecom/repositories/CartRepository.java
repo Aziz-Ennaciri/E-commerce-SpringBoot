5cb6b7d4721a5407cb9e19eb968229bee98aeee7
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 18:17:09 +0100

    modifying CategoryRepository , the query findCartsByProductId

--- src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
@@ -12,4 +14,7 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId=?2")
     Cart findCartByEmailAndCartId(String emailId, Long cartId);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems ci JOIN FETCH ci.product p WHERE p.productId = ?1")
+    List<Cart> findCartsByProductId(Long productId);
 }
