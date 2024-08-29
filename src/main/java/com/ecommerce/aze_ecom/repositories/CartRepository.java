b1194a277b7fa6ab73a7dcbe98fbce1d1e192ea0
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 01:24:26 +0100

    add new query findCartByEmailAndCartId

--- src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/CartRepository.java
@@ -9,4 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface CartRepository extends JpaRepository<Cart,Long> {
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
     Cart findCartByEmail(String email);
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId=?2")
+    Cart findCartByEmailAndCartId(String emailId, Long cartId);
 }
