1e6ff344e0f14ef795e1e577eab939bfe691ab9a
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 19:49:07 +0100

    Creating AddressRepository

--- src/main/java/com/ecommerce/aze_ecom/repositories/AddressRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/AddressRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.aze_ecom.repositories;
 
 import com.ecommerce.aze_ecom.beans.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
 }
