82f26f216eee362b3e58a68c6e25746e208dd548
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 18:13:41 +0100

    modifying CartController

--- src/main/java/com/ecommerce/aze_ecom/controller/CartController.java
+++ src/main/java/com/ecommerce/aze_ecom/controller/CartController.java
@@ -53,8 +53,8 @@ public class CartController {
     }
 
     @DeleteMapping("/carts/{cartId}/product/{productId}")
-    public ResponseEntity<String> deleteProductFromCart(@PathVariable Long cartId,@PathVariable Long productId){
-        String status = cartService.deleteProductFromCart(cartId,productId);
-        return new ResponseEntity<String>(status,HttpStatus.OK);
+    public ResponseEntity<String> deleteProductFromCart(@PathVariable Long cartId, @PathVariable Long productId) {
+        String status = cartService.deleteProductFromCart(cartId, productId);
+        return new ResponseEntity<String>(status, HttpStatus.OK);
     }
 }
