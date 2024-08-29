28d2aec8e07ea8338103e513ab213c80c3fa13c6
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 19:48:18 +0100

    Creating AddressDTO and AddressRepository

---
+++ src/main/java/com/ecommerce/aze_ecom/playload/AddressDTO.java
@@ -0,0 +1,18 @@
+package com.ecommerce.aze_ecom.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+    private Long addressId;
+    private String street;
+    private String buildingName;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
+}
---
+++ src/main/java/com/ecommerce/aze_ecom/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.aze_ecom.repositories;
+
+import com.ecommerce.aze_ecom.beans.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
