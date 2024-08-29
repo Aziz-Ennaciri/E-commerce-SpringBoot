c89a292c1e2f9573f294117a9928a2674531a65c
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-29 04:22:21 +0100

    add annotation @Modifying and implement a Query

--- src/main/java/com/ecommerce/aze_ecom/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/aze_ecom/repositories/CartItemRepository.java
@@ -2,11 +2,16 @@ package com.ecommerce.aze_ecom.repositories;
 
 import com.ecommerce.aze_ecom.beans.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
-    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
+
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
