bd14994798f8832eca7b997ee572b3469c5d46c8
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-30 03:00:21 +0100

    creating AddressMapper

---
+++ src/main/java/com/ecommerce/aze_ecom/mappers/AddressMapper.java
@@ -0,0 +1,33 @@
+package com.ecommerce.aze_ecom.mappers;
+
+import com.ecommerce.aze_ecom.beans.Address;
+import com.ecommerce.aze_ecom.playload.AddressDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressMapper {
+
+    public AddressDTO toDto(Address address){
+        AddressDTO addressDTO = new AddressDTO();
+        addressDTO.setAddressId(address.getAddressId());
+        addressDTO.setStreet(address.getStreet());
+        addressDTO.setBuildingName(address.getBuildingName());
+        addressDTO.setCity(address.getCity());
+        addressDTO.setState(address.getState());
+        addressDTO.setCountry(address.getCountry());
+        addressDTO.setPincode(address.getPincode());
+        return addressDTO;
+    }
+    public Address toEntity(AddressDTO addressDTO){
+        Address address = new Address();
+        address.setAddressId(addressDTO.getAddressId());
+        address.setStreet(addressDTO.getStreet());
+        address.setBuildingName(addressDTO.getBuildingName());
+        address.setCity(addressDTO.getCity());
+        address.setState(addressDTO.getState());
+        address.setCountry(addressDTO.getCountry());
+        address.setPincode(addressDTO.getPincode());
+        return address;
+    }
+
+}
